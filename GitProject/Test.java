7d8d75c1bff70a9808ce28979897b802d8f9ecf6
lakshmipath <pathi.devops75@gmail.com>
2022-10-01 06:28:12 -0700

    First Commit

---
+++ GitProject/Test.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Test 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter x value:");
+		int x=sc.nextInt();
+		System.out.print("Enter y value:");
+		int y=sc.nextInt();
+		System.out.println(x+"+"+y+"="+(x+y));
+
+	}
+}
